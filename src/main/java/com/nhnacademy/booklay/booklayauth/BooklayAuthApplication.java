16a1e572d4c7a886b75769123b01efbcaa752c61
Com-Sun <gnlslfflffl@gmail.com>
2023-01-13 13:53:47 +0900

    Initialize Repository

---
+++ src/main/java/com/nhnacademy/booklay/booklayauth/BooklayAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.booklay.booklayauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklayAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooklayAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/booklay/booklayauth/BooklayAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.booklay.booklayauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklayAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
